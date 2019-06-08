b7af8fa87992a58f9828a2a15b3e49a0d52302d4
Miguel Angel Hernandez Cruz <mhernandez@unphu.edu.do>
2019-06-07 23:16:36 -0400

    Entity modified to take into account the date of creation and update of the product

--- symfony/src/Entity/Product.php
+++ symfony/src/Entity/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use phpDocumentor\Reflection\DocBlock\Tags\Param;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -43,6 +44,22 @@ class Product implements \JsonSerializable
      * @ORM\JoinColumn(nullable=false)
      */
     private $category;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    public function __construct()
+    {
+      // Automatically set the creation date of each new product.
+      $this->setCreatedAt(new \DateTime());
+    }
 
     public function getId(): ?int
     {
@@ -123,6 +140,43 @@ class Product implements \JsonSerializable
       'name' => $this->name,
       'price' => $this->price,
       'category' => $this->category,
+      'created_at' => $this->createdAt,
     ];
+  }
+
+  public function getCreatedAt(): ?\DateTimeInterface
+  {
+      return $this->createdAt;
+  }
+
+  public function setCreatedAt(\DateTimeInterface $createdAt): self
+  {
+      $this->createdAt = $createdAt;
+
+      return $this;
+  }
+
+  public function getUpdatedAt(): ?\DateTimeInterface
+  {
+      return $this->updatedAt;
+  }
+
+  public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+  {
+      $this->updatedAt = $updatedAt;
+
+      return $this;
+  }
+
+  /**
+   * We invoke the { updatedTimestamps } method to set automatically
+   * every time a product is authenticated.
+   */
+  public function updatedTimestamps(): void
+  {
+    $this->setUpdatedAt(new \DateTime('now'));
+    if ($this->getCreatedAt() === null) {
+      $this->setCreatedAt(new \DateTime('now'));
+    }
   }
 }
